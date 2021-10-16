8ef02437a97d7e5fa4da1e8057f4828b71555bf2
Murat ÇAY <murtcay@gmail.com>
2021-10-16 16:18:35 +0300

    routing for mvc

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const pageRoute = require('./routes/pageRoute');
+
 const app = express();
 
 // Template engine
@@ -9,19 +11,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.status(200).render('index', {
-        page_name:'index'
-    });
-})
+app.use('/', pageRoute);
 
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: 'about'
-    });
-})
+// app.get('/about', );
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is litening on port ${port}`);
-})
+});
