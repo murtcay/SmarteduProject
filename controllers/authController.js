44a657bf5ea07da5b1ac135db439f7f1ae11b21d
Murat ÇAY <murtcay@gmail.com>
2021-10-17 17:43:55 +0300

    user dashboard page and logout link fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ exports.loginUser = async (req, res) => {
                 (err, same) => {
                     if(same) {
                         req.session.userID = user._id;
-                        res.status(200).redirect('/');
+                        res.status(200).redirect('/users/dashboard');
                     }
                     else {
                         res.status(400).json({
@@ -54,5 +54,14 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
+    });
+};
+
+exports.getDashboardPage = async (req, res) => {
+    const user = await User.findOne({_id: req.session.userID});
+
+    res.status(200).render('dashboard', {
+        page_name: 'dashboard', 
+        user: user
     });
 };
