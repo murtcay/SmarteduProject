74c9736ee01aac00b82b5c761cd17f8b3da082cd
Murat ÇAY <murtcay@gmail.com>
2021-10-18 02:59:46 +0300

    course search box regex operation

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -24,15 +24,35 @@ exports.getAllCourses = async (req, res) => {
     try {
 
         const categorySlug = req.query.categories;
-        const category = await Category.findOne({slug: categorySlug});
-
+        
+        const query = req.query.search;
+    
+        const category = await Category.findOne({slug:categorySlug})
+    
         let filter = {};
-
+    
         if(categorySlug) {
-            filter = {category: category._id};
+          filter = {category:category._id}
         }
+    
+        if(query) {
+          filter = {name:query}
+        }
+    
+        if(!query && !categorySlug) {
+          filter.name = "",
+          filter.category = null
+        }
+        
+        const courses = await Course.find({
+          $or:[
+            {
+                name: { $regex: '.*' + filter.name + '.*', $options: 'i'}
+            },
+            {category: filter.category}
+          ]
+        }).sort('-createdAt').populate('user');
 
-        const courses = await Course.find(filter).sort('-dateCreated');
         const categories = await Category.find();
 
         res.status(200).render('courses', {
@@ -52,10 +72,13 @@ exports.getCourse = async (req, res) => {
     try {
         const user = await User.findById(req.session.userID);
         const course = await Course.findOne({slug: req.params.slug}).populate('user');
+        const categories = await Category.find();
+
         res.status(200).render('course-single', {
             course: course,
             page_name: 'courses',
-            user: user
+            user: user,
+            categories: categories
         });
     } catch (error) {
         res.status(400).json({
